1752df7ad93776635c2ad0ff3871e1a333b85845
Philippe Muller <philippe.muller@gmail.com>
2013-12-09 02:50:49 +0100

    Add classifiers for Python 3.2 and 3.3

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Software Distribution',
