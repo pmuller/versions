41db0dc0e43ff9cd699bea0c1cfa671e2ed1d504
Philippe Muller <philippe.muller@gmail.com>
2013-12-14 16:54:24 +0100

    Added Package.parse

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
-from versions.packages import Package
+from versions.packages import Package, InvalidPackageExpression, \
+    InvalidPackageInfo
+from versions.requirements import Requirement
 
 
 class TestPackage(TestCase):
@@ -9,3 +11,16 @@ class TestPackage(TestCase):
         package = Package('foo', 1)
         self.assertEqual(hash(package),
                          hash(package.name) ^ hash(package.version))
+
+    def test_parse(self):
+        p1 = Package.parse(' foo - 1.2 ')
+        self.assertEqual(p1.name, 'foo')
+        self.assertEqual(p1.version, '1.2.0')
+        p2 = Package.parse(' foo - 1.2 ; depends bar >2,<3,>2.5; depends baz')
+        self.assertTrue(Requirement.parse('bar >2,<3,>2.5') in p2.dependencies)
+        self.assertTrue(Requirement.parse('baz') in p2.dependencies)
+        self.assertEqual(len(p2.dependencies), 2)
+
+    def test_parse_raises(self):
+        self.assertRaises(InvalidPackageExpression, Package.parse, '#$!@')
+        self.assertRaises(InvalidPackageInfo, Package.parse, 'foo-1;#$!@')
--- versions/packages.py
+++ versions/packages.py
@@ -1,3 +1,64 @@
+import re
+
+from .requirements import Requirement
+from .version import Version
+from .errors import Error
+
+
+RE = re.compile(r"""
+^
+\s*
+(?P<name>[A-Za-z0-9_\-]+)
+\s*
+-
+\s*
+(?P<version>
+    # major version number
+    \d+
+    (?:
+        \.
+        # minor version number
+        \d+
+        (?:
+            \.
+            # patch version number
+            \d+
+        )?
+    )?
+    (?:
+        -
+        # pre-release version number
+        [0-9a-zA-Z.-]*
+    )?
+    (?:
+        \+
+        # build metadata
+        [0-9a-zA-Z.-]*
+    )?
+)
+\s*
+$
+""", re.X)
+
+
+class InvalidPackageExpression(Error):
+    """Raised failing to parse a package expression.
+    """
+    def __init__(self, expression):
+        self.expression = expression
+        message = 'Invalid package expression: %s' % expression
+        super(InvalidPackageExpression, self).__init__(message)
+
+
+class InvalidPackageInfo(Error):
+    """Raised failing to parse a package expression info.
+    """
+    def __init__(self, info):
+        self.info = info
+        message = 'Invalid package expression info: %s' % info
+        super(InvalidPackageInfo, self).__init__(message)
+
+
 class Package(object):
     """A package.
 
@@ -6,11 +67,39 @@ class Package(object):
     :type version: :class:`Version`
 
     """
-    def __init__(self, name, version):
-        #: Package name
+    def __init__(self, name, version, dependencies=None):
+        #: Package name.
         self.name = name
-        #: Package version
+        #: Package version.
         self.version = version
+        #: ``set`` of :class:`Requirement` objects
+        self.dependencies = dependencies or set()
 
     def __hash__(self):
         return hash(self.name) ^ hash(self.version)
+
+    @classmethod
+    def parse(self, package_expression):
+        """Parse a ``package_expression`` into a :class:`Package` object.
+        """
+        parts = re.split(r'\s*;\s*', package_expression)
+        name_ver_str = parts[0]
+        infos = parts[1:]
+        dependencies = set()
+
+        name_ver_match = RE.match(name_ver_str)
+        if not name_ver_match:
+            raise InvalidPackageExpression(name_ver_str)
+
+        name, version_str = name_ver_match.groups()
+        version = Version.parse(version_str)
+
+        for info in infos:
+            if info.startswith('depends '):
+                dependency_str = info.split(' ', 1)[1]
+                dependency = Requirement.parse(dependency_str)
+                dependencies.add(dependency)
+            else:
+                raise InvalidPackageInfo(info)
+
+        return Package(name, version, dependencies)
