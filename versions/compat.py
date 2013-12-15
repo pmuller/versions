6b561cb632084f3116812d87aef475908800f1b5
Philippe Muller <philippe.muller@gmail.com>
2013-12-15 23:14:41 +0100

    don't care about coverage for version compat code

--- versions/compat.py
+++ versions/compat.py
@@ -5,6 +5,6 @@ MAJOR = sys.version_info[0]
 
 
 if MAJOR == 3:
-    cmp = lambda a, b: (a > b) - (a < b)
+    cmp = lambda a, b: (a > b) - (a < b)  # pragma: no cover
 else:  # hopefully MAJOR == 2
-    cmp = cmp
+    cmp = cmp  # pragma: no cover
