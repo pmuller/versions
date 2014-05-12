8d03046c2f387d2d2fd944724c1de56976077aaa
Philippe Muller <philippe.muller@gmail.com>
2014-05-12 16:15:19 +0200

    fix tests for python 3

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,3 +1,4 @@
+import sys
 from unittest import TestCase
 
 from versions.compat import basestring
@@ -7,5 +8,7 @@ class TestBaseString(TestCase):
 
     def test(self):
         self.assertTrue(isinstance('foo', basestring))
-        self.assertTrue(isinstance(u'foo', basestring))
         self.assertFalse(isinstance(42, basestring))
+
+        if sys.version_info[0] == 2:
+            self.assertTrue(isinstance(u'foo', basestring))
